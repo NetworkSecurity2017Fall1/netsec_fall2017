47e571ff3bf37206fa8ccef626ddaac2ccc8b227
harrylaw <harrylaw86@gmail.com>
2017-10-09 20:56:54 -0400

    pkt.Type should be set to 5 for Data Packets

--- lab2/src/lab2_protocol/Transport.py
+++ lab2/src/lab2_protocol/Transport.py
@@ -6,16 +6,16 @@ from . import Packets
 
 class MyProtocolTransport(StackingTransport):
     def write(self, data):
-    # this will be the data from the upper layer
+        # this will be the data from the upper layer
         chunk_size = 1024
         counter = 0
-        MyProtocolPackets = []
-        while( len(data) > 0 ):
+        my_protocol_packets = []
+        while len(data) > 0:
             pkt = Packets.PEEPPacket()
-            pkt.Type = 6
+            pkt.Type = 5
             pkt.SequenceNumber = counter
-            pkt.Acknowledgemen = 0
-            if( len(data) > chunk_size ):
+            pkt.Acknowledgement = 0
+            if len(data) > chunk_size:
                 pkt.Data = data[:chunk_size]
                 data = data[chunk_size:]
             else:
@@ -23,8 +23,8 @@ class MyProtocolTransport(StackingTransport):
                 data = data[len(data):]
             pkt.Checksum = pkt.calculateChecksum()
             counter = counter + 1
-            MyProtocolPackets.append(pkt)
+            my_protocol_packets.append(pkt)
 
         # Create MyProtocolPackets
-        for pkt in MyProtocolPackets:
+        for pkt in my_protocol_packets:
             self.lowerTransport().write(pkt.__serialize__())
