74cb60ba8f929f7ea9a2f326189b94d82718868c
Yu-TSern <leojoe123321@gmail.com>
2017-10-10 10:40:04 -0400

    fix conflict

--- lab2/src/lab2_protocol/Transport.py
+++ lab2/src/lab2_protocol/Transport.py
@@ -1,18 +1,21 @@
+"""Transport"""
+
 from playground.network.common import StackingTransport
 from . import Packets
-class MyProtocolTransport(StackingTransport):
 
+
+class MyProtocolTransport(StackingTransport):
     def write(self, data):
-    # this will be the data from the upper layer
+        # this will be the data from the upper layer
         chunk_size = 1024
         counter = 0
-        MyProtocolPackets = []
-        while( len(data) > 0 ):
+        my_protocol_packets = []
+        while len(data) > 0:
             pkt = Packets.PEEPPacket()
-            pkt.Type = 6
+            pkt.Type = 5
             pkt.SequenceNumber = counter
-            pkt.Acknowledgemen = 0
-            if( len(data) > chunk_size ):
+            pkt.Acknowledgement = 0
+            if len(data) > chunk_size:
                 pkt.Data = data[:chunk_size]
                 data = data[chunk_size:]
             else:
@@ -20,8 +23,9 @@ class MyProtocolTransport(StackingTransport):
                 data = data[len(data):]
             pkt.Checksum = pkt.calculateChecksum()
             counter = counter + 1
-            MyProtocolPackets.append(pkt)
+            my_protocol_packets.append(pkt)
 
         # Create MyProtocolPackets
-        for pkt in MyProtocolPackets:
+        for pkt in my_protocol_packets:
             self.lowerTransport().write(pkt.__serialize__())
+
