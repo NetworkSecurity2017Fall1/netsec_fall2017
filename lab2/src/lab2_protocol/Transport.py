0b660be40b96dd0c2f44916a3ade7eb0882da2ef
Yu-TSern <leojoe123321@gmail.com>
2017-10-12 21:35:14 -0400

    fix seq

--- lab2/src/lab2_protocol/Transport.py
+++ lab2/src/lab2_protocol/Transport.py
@@ -12,20 +12,19 @@ class MyProtocolTransport(StackingTransport):
     def write(self, data):
         # this will be the data from the upper layer
         chunk_size = 1024
-        counter = 0
         my_protocol_packets = []
         while len(data) > 0:
             pkt = Packets.PEEPPacket()
             pkt.Type = 5
-            pkt.SequenceNumber = self.seq_start + counter
+            pkt.SequenceNumber = self.seq_start
             if len(data) > chunk_size:
+                self.seq_start += chunk_size
                 pkt.Data = data[:chunk_size]
                 data = data[chunk_size:]
-                counter = counter + 1024
             else:
+                self.seq_start += len(data)
                 pkt.Data = data[:len(data)]
                 data = data[len(data):]
-                counter = counter + len(data)
             pkt.Checksum = pkt.calculateChecksum()
             my_protocol_packets.append(pkt)
 
