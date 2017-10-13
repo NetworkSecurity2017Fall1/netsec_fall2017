198f8d0b94c1255520188c60f4f18bb28d726ba3
Yongqiang Fan <fldclassicblue@gmail.com>
2017-10-13 07:22:32 -0400

    send ack and gitignore

--- lab2/src/lab2_protocol/ReliableTransmission.py
+++ lab2/src/lab2_protocol/ReliableTransmission.py
@@ -1,49 +1,74 @@
 """ReliableTransmission"""
 
-from .Packets import PEEPPacket
+from Packets import PEEPPacket
 import random
 
 
-class ReliableTransmission:
+class AckValidate:
     def __init__(self, seq):
         self.pktReceived = []
         self.WindowSize = 5
-        self.curr = 0
         self.next = seq
 
-    # def slidingWindowExamine(self):
-    #     if self.pktReceived[self.curr].SequenceNumber - self.pktReceived[self.curr + 5].SequenceNumber == self.WindowSize-1:
-    #         return 0, self.pktReceived
-    #     else:
-    #         missingNumber = []
-    #         seqNum = self.pktReceived[0].SequenceNumber
-    #         for pkt in self.pktReceived:
-    #             if seqNum != pkt.SequenceNumber:
-    #                 missingNumber.append(seqNum)
-    #             seqNum += 1
-    #         return 2, missingNumber
-
     def addPackets2Queue(self, packet):
-        self.pktReceived.append(packet)
-        self.sortPacketBySeqNum()
-        if packet.SequenceNumber == self.next:
-            return True
+        shift = 0
+        if packet.SequenceNumber in range(self.next, self.next + self.WindowSize):
+            self.pktReceived.append(packet)
+            self.sortPacketBySeqNum()
+        while self.pktReceived and self.pktReceived[0].SequenceNumber == self.next:
+            self.next += 1
+            shift += 1
+            self.pktReceived.pop(0)
+
+        return shift
 
 
     def sortPacketBySeqNum(self):
         self.pktReceived.sort(key=lambda pkt: pkt.SequenceNumber, reverse=False)
-        return self.pktReceived
+        # return self.pktReceived
+
+
+class SendAck:
+
+    def __init__(self, seq):
+        self.time_limit = 0.5
+        self.WindowSize = 5
+        self.pktReceived = []
+        self.next = seq
+
+
+    def addPackets2Queue(self, packet):
+        if packet.SequenceNumber in range(self.next, self.next + self.WindowSize):
+            self.pktReceived.append(packet)
+
+
+    def sortPacketBySeqNum(self):
+        self.pktReceived.sort(key=lambda pkt: pkt.SequenceNumber, reverse=False)
+
+    def updateAck(self):
+        self.sortPacketBySeqNum()
+        while self.pktReceived and self.pktReceived[0].SequenceNumber == self.next:
+            self.next += 1
+            self.pktReceived.pop(0)
+
+        return self.next
+
 
 
 if __name__ == "__main__":
-    reliableTransmission = ReliableTransmission()
-    for i in range(0,5):
+    #reliableTransmission = AckValidate(1)
+    reliableTransmission = SendAck(1)
+    test_list = [1,2,4,5,1,3]
+    for i in range(0, len(test_list)):
         pkt = PEEPPacket()
         pkt.Type = 1
-        pkt.SequenceNumber = random.randint(0,10)
-        #pkt.SequenceNumber = i
+        # pkt.SequenceNumber = random.randint(0,10)
+        pkt.SequenceNumber = test_list[i]
         pkt.Acknowledgement = 1
         pkt.Checksum = pkt.calculateChecksum()
-        state, result = reliableTransmission.addPackets2Queue(pkt)
-        print(state, result)
-    print([pkt.SequenceNumber for pkt in reliableTransmission.sortPacketBySeqNum()])
+        #result =
+        reliableTransmission.addPackets2Queue(pkt)
+        result = reliableTransmission.updateAck()
+        print(reliableTransmission.pktReceived)
+        print(result)
+    #print([pkt.SequenceNumber for pkt in reliableTransmission.sortPacketBySeqNum()])
