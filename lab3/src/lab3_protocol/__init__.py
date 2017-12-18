7621541eeaae2c456eb58d6938e2d32fe0981c12
Yu-Tsern <leojoe123321@gmail.com>
2017-12-18 16:34:41 -0500

    Update __init__.py

--- lab3/src/lab3_protocol/__init__.py
+++ lab3/src/lab3_protocol/__init__.py
@@ -5,7 +5,7 @@ from playground.network.common import StackingProtocolFactory
 from . import PEEPProtocols
 from . import PLSProtocols
 
-team5ClientFactory = StackingProtocolFactory(lambda: PLSProtocols.PLSClientProtocol(), lambda: PEEPProtocols.PEEPClientProtocol())
-team5ServerFactory = StackingProtocolFactory(lambda: PLSProtocols.PLSServerProtocol(), lambda: PEEPProtocols.PEEPServerProtocol())
+team5ClientFactory = StackingProtocolFactory(lambda: PEEPProtocols.PEEPClientProtocol(), lambda: PLSProtocols.PLSClientProtocol())
+team5ServerFactory = StackingProtocolFactory(lambda: PEEPProtocols.PEEPServerProtocol(), lambda: PLSProtocols.PLSServerProtocol())
 team5Connector = playground.Connector(protocolStack=(team5ClientFactory, team5ServerFactory))
 playground.setConnector("team5_protocol", team5Connector)
